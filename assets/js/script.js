ea883e6ce787d2c047469668e51a2ecf82cc9226
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-02-01 11:18:46 +0100

    Structure java script document with docstrings.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,66 +7,49 @@ const playerCard = document.getElementById("player-card");
 const computerCard = document.getElementById("computer-card");
 let showingRules = false;
 
-// Load the DOM before running the game
-// Get the button elements and attach event listeners to them
-
+/**
+ * Event listener loading the DOM before running the game.
+ * Click event listeners attaches to game buttons and the rules toggle button.
+ */
 document.addEventListener("DOMContentLoaded", function() {
-    // Return all of the buttons on the page
     let buttons = document.getElementsByClassName("btn");
-
-    // Return each element in the buttons array on iteration
     for (let button of buttons) {
-        // Listen for clicked button
         button.addEventListener("click", function() {
-            // Call main game function
             let playerChoice = this.getAttribute("data-choice");
                 runGame(playerChoice);
       });
     }       
-
     document.getElementById('toggle-rules').addEventListener('click', toggleRules);
-
 });
 
-// Toggle function
-
+/**
+ * Toggle function enables and disables visibility of the rules section and updates the button icon.
+ */
 function toggleRules() {
   let rulesElement = document.getElementById('rules');
-  let toggleButton = document.getElementById('toggle-rules');
-
   if (showingRules === false) {
     rulesElement.classList.remove('hidden');
-    toggleButton.querySelector('.fa-circle-plus').remove();
-    toggleButton.insertAdjacentHTML('beforeend', '<i class="fa-solid fa-circle-minus"></i>');
+    document.getElementById('toggle-rules').insertAdjacentHTML('beforeend', '<i class="fa-solid fa-circle-minus"></i>');
     showingRules = true;
   } else {
     rulesElement.classList.add('hidden');
-    toggleButton.querySelector('.fa-circle-minus').remove();
-    toggleButton.insertAdjacentHTML('beforeend', '<i class="fa-solid fa-circle-plus"></i>');
+    document.getElementById('toggle-rules').querySelector('.fa-circle-minus').remove();
     showingRules = false;
   }
 }
 
-// Main function accepts datachoice value of buttons
-
+/**
+ * Main function accepts datachoice value of buttons and the runs the game with the player's choice as input.
+ * It generates a random computer choice, updates player and computer card/image, and then checks for the winner.
+ */
 function runGame(playerChoice) {
-
-    // Change player card to corresponding image 
     playerCard.src = `assets/images/icons/player/${choices[playerChoice]}-p.png`;
     playerCard.alt = choices[playerChoice];
-
-    // Generate random number for computer choice
     let computerChoice = Math.floor(Math.random() * choices.length);
-
-    // Change computer card to corresponding image
     computerCard.src = `assets/images/icons/computer/${choices[computerChoice]}-c.png`;
     computerCard.alt = choices[computerChoice];
-
-    // Check winner
     let result = checkWinner(choices[computerChoice], choices[playerChoice]);
-    
     updateScores(result);
-
 } 
 
 // Compare choices and determine the winner
@@ -74,7 +57,6 @@ function checkWinner(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
       return 'tie';
-
     } else if (
       (playerChoice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
       (playerChoice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
@@ -82,33 +64,28 @@ function checkWinner(playerChoice, computerChoice) {
       (playerChoice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
       (playerChoice === 'spock' && (computerChoice === 'scissors' || computerChoice === 'rock'))
     ) {
-
       return 'player';
-
     } else {
-        
       return 'computer';
     }
   }
 
-// Update result to winnner/loser/tie message
+/**
+ * Determines the outcome of each turn and returns message based in result.
+ */
 function updateScores(result) {
-
     if (result === 'player') {
       incrementPlayerScore();
       winnerLoser.textContent = 'You win!';
-
     } else if (result === 'computer') {
       incrementComputerScore();
       winnerLoser.textContent = 'Computer wins!';
-      
     } else {
       winnerLoser.textContent = "It's a tie!";
     }
   }
  
-/* Feature left to implement
-// Return message describing choice relationship and outcome
+/* Feature left to implement -> Return message describing choice relationship and outcome.
 function getComputerReaction(choice1, choice2) {
     
     const rules = {
@@ -133,26 +110,30 @@ function getComputerReaction(choice1, choice2) {
         rock: "Spock vaporizes Rock",
       },
     };
-  
     return rules[choice1][choice2] || "";
-  }
-  
-*/
+} */
 
+
+/**
+ * Increments score by 1 if player wins.
+ */
 function incrementPlayerScore() {
-
   let score = parseInt(playerScore.textContent);
   playerScore.textContent = score + 1;
 }
 
+/**
+ * Increments score by 1 if computer wins.
+ */
 function incrementComputerScore() {
-
   let score = parseInt(computerScore.textContent);
   computerScore.textContent = score + 1;
 }
 
+/**
+ * Resets score to 0 and removes winner, loser and tie message.
+ */
 function resetScore() {
-
   document.getElementById('player-score').innerText = '0';
   document.getElementById('computer-score').innerText = '0';
   winnerLoser.textContent = "\u00A0";
